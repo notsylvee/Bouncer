b96d681040112b6973b41b3414c11af75ccfe0fc
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-07-21 23:40:21 -0400

    defined fs

--- src/events/taunt.js
+++ src/events/taunt.js
@@ -1,21 +1,21 @@
-const { PermissionFlagsBits } = require("discord.js");
+const fs = require("fs/promises");
 
 module.exports = {
-    name: "messageCreate",
-    async execute(message) {
+  name: "messageCreate",
+  async execute(message) {
 
-        if (message.author.bot || !message.guild) return;
+    if (message.author.bot || !message.guild) return;
         
-        const tauntsJsonData = await fs.readFile("data/taunts.json", {encoding: "utf8"});
-        const tauntsMap = JSON.parse(tauntsJsonData);
-        const taunts = tauntsMap["taunts"];
-        const taunt = taunts[Math.floor(Math.random() * taunts.length)];
+    const tauntsJsonData = await fs.readFile("data/taunts.json", {encoding: "utf8"});
+    const tauntsMap = JSON.parse(tauntsJsonData);
+    const taunts = tauntsMap["taunts"];
+    const taunt = taunts[Math.floor(Math.random() * taunts.length)];
 
-        const chance = Math.random() * 500;
-        if (chance < 497) {
-            return;
-          } else {
-            message.channel.send(`<:Bouncer:1395295204163981404> ${taunt.text}`).catch((err) => {return});
-          };
-    },
+    const chance = Math.random() * 500;
+    if (chance < 497) {
+      return;
+    } else {
+      message.channel.send(`<:Bouncer:1395295204163981404> ${taunt.text}`).catch((err) => {return});
+    };
+  },
 };
